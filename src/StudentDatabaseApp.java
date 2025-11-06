a6f75b003f1814fab462a66034f8db961076a761
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-11-06 16:59:46 -0500

    Implemented function to connect to PostgreSQL, inspired by lecture example.

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -9,4 +9,35 @@ import java.sql.*;
  * @author Mahdi Bouakline
  * @date November 2025
  */
-public class StudentDatabaseApp {}
+public class StudentDatabaseApp {
+    // Database connection credentials
+    // Don't really remember my credentials, hoping these are correct
+    private static final String URL = "jdbc:postgresql://localhost:5432/students_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Mahdi0610";
+    
+    /**
+     * Establishes and returns a connection to the PostgreSQL database
+     * 
+     * @return Connection object to the database
+     * @throws SQLException if connection fails
+     */
+    public static Connection connect() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            
+            // Connect to the database
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+            if (conn != null) {
+                System.out.println("Connected to PostgreSQL successfully!");
+            }
+            return conn;
+            
+        } catch (ClassNotFoundException e) {
+            System.err.println("PostgreSQL JDBC Driver not found.");
+            e.printStackTrace();
+            throw new SQLException("Driver not found");
+        }
+    }
+}
