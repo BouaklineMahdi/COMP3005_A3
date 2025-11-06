3bd92c84641937f5f87f5b72d89f8c9816690a0b
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-11-06 16:47:57 -0500

    Initial Class declaration.

---
+++ src/StudentDatabaseApp.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+
+/**
+ * COMP3005 Assignment 3 - Database Interaction with PostgreSQL and JDBC
+ * 
+ * This application demonstrates CRUD operations on a PostgreSQL database
+ * using JDBC (Java Database Connectivity).
+ * 
+ * @author Mahdi Bouakline
+ * @date November 2025
+ */
+public class StudentDatabaseApp {}
